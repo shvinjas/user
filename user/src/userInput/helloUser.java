0de4b177aafe2971b0a60784624dc74bdeb15a4d
Gloria <Gloria@Gloria-PC>
2023-01-01 03:47:51 +1300

    first commit 2023

---
+++ user/src/userInput/helloUser.java
@@ -0,0 +1,14 @@
+package userInput;
+import java.util.Scanner;
+
+public class helloUser {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = sc.next();
+		System.out.println("Hello" + " " +name+","+ "2023 will be holy spirit annointing year for you"); 
+		
+	}
+
+}
